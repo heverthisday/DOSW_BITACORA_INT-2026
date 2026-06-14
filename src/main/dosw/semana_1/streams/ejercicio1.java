9e78bbc20df5b48f75c3789833750e7b32276645
heverthisday <heverbarrerabatero3@gmail.com>
2026-06-14 06:50:27 -0500

    primer ejercicio

---
+++ src/main/dosw/semana_1/streams/ejercicio1.java
@@ -0,0 +1,19 @@
+package main.dosw.semana_1.streams;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class ejercicio1 {
+    static void numero1() {
+        List<Integer> numeros = List.of(3, 8, 10, 12, 15, 18, 20);
+        List<Integer> mayoresPar = numeros.stream().filter(n -> n > 10 && n % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(mayoresPar);
+    }
+
+    public static void main(String[] args) {
+        numero1();
+    }
+
+
+}
