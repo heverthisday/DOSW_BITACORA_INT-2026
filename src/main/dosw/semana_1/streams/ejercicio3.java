8988ccefc44e1994fdf54f026106b7c53662fafd
heverthisday <heverbarrerabatero3@gmail.com>
2026-06-14 10:30:27 -0500

    ejercicio 4

--- src/main/dosw/semana_1/streams/ejercicio3.java
+++ src/main/dosw/semana_1/streams/ejercicio3.java
@@ -5,6 +5,7 @@ import java.util.stream.*;
 
 public class ejercicio3 {
 
+
     static class User {
         int id;
         String name;
@@ -17,26 +18,33 @@ public class ejercicio3 {
             this.age = age;
             this.active = active;
         }
+
         public String getName() {
             return name;
         }
+
         public boolean isActive() {
             return active;
-        }
-    public static void main(String[] args){
-        List <User> users = List.of(
-                new User(1,"juan",20,true),
-                new User(2,"ana",22,false),
-                new User(3,"carlos",19,true),
-                new User(4,"maria",25,true)
-        );
-
-        List<String> activos = users.stream().filter(user -> user.isActive()).map(User::getName).
-                map(name->name.toUpperCase()).sorted().collect(Collectors.toList());
-
-        System.out.println(activos);
         }
     }
 
 
+    public static List<User> users = List.of(
+            new User(1, "juan", 15, true),
+            new User(2, "ana", 22, false),
+            new User(3, "carlos", 19, true),
+            new User(4, "maria", 25, true)
+    );
+
+    public static void main(String[] args) {
+
+        List<String> activos = users.stream()
+                .filter(User::isActive)
+                .map(User::getName)
+                .map(String::toUpperCase)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(activos);
+    }
 }
