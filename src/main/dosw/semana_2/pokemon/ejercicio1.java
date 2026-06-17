d208b377804c8d2a9cd7a0d25eab633587b85ee8
heverthisday <heverbarrerabatero3@gmail.com>
2026-06-17 10:49:34 -0500

    ejercicio 4

--- src/main/dosw/semana_2/pokemon/ejercicio1.java
+++ src/main/dosw/semana_2/pokemon/ejercicio1.java
@@ -6,22 +6,25 @@ public class ejercicio1 {
     static class Pokemon{
         String name;
         String type;
+        int level;
 
-        public Pokemon(String name, String type){
+        public Pokemon(String name, String type, int level) {
             this.name=name;
             this.type=type;
+            this.level=level;
         }
         public String getName(){return name;}
         public String getType(){return type;}
+        public int getLevel(){return level;}
     }
 
     public static List<Pokemon> pokemons = List.of(
-            new Pokemon("Pikachu","Electrico"),
-            new Pokemon("Charmarder","Fuego"),
-            new Pokemon("Squirtle","Agua"),
-            new Pokemon("Vulpix","Fuego"),
-            new Pokemon("Bulbasaur","Planta"),
-            new Pokemon("Flareon","Fuego")
+            new Pokemon("Pikachu","Electrico",45),
+            new Pokemon("Charmarder","Fuego",62),
+            new Pokemon("Squirtle","Agua",38),
+            new Pokemon("Vulpix","Fuego",71),
+            new Pokemon("Bulbasaur","Planta",55),
+            new Pokemon("Flareon","Fuego",29)
     );
 
     public static void main(String[] args){
--- src/main/dosw/semana_2/pokemon/ejercicio3.java
+++ src/main/dosw/semana_2/pokemon/ejercicio3.java
@@ -2,19 +2,16 @@ package main.dosw.semana_2.pokemon;
 
 import java.util.*;
 import java.util.stream.*;
+import static main.dosw.semana_2.pokemon.ejercicio1.pokemons;
 
 public class ejercicio3 {
 
-    static void niveles () {
-
-        List<Integer> niveles = List.of(45, 62, 38, 71, 55, 29);
-        int suma = niveles.stream().reduce(0, Integer::sum);
-        System.out.println( " suma total de niveles " + suma);
-
-    }
-
-
     public static void main(String[] args) {
-        niveles();
+
+        int total = pokemons.stream()
+                .mapToInt(ejercicio1.Pokemon::getLevel)
+                .sum();
+
+        System.out.println(total);
     }
 }
