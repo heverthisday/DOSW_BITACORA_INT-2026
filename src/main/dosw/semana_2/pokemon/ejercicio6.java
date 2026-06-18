71bc92a235f0c2fd34474383d7b0423d116e77aa
heverthisday <heverbarrerabatero3@gmail.com>
2026-06-17 20:12:10 -0500

    ejercicio 6

---
+++ src/main/dosw/semana_2/pokemon/ejercicio6.java
@@ -0,0 +1,14 @@
+package main.dosw.semana_2.pokemon;
+
+import java.util.*;
+import java.util.stream.*;
+import static main.dosw.semana_2.pokemon.ejercicio1.pokemons;
+
+public class ejercicio6 {
+    public static void main(String[] args) {
+        List<String> nombres = pokemons.stream()
+                .map(ejercicio1.Pokemon::getName)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+}
